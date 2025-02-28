a45153c356732961b933b7d2c67f5029547c13c8
Nayron <nayron.campos@sga.pucminas.br>
2025-02-28 00:49:14 -0300

    Arquivo conexao

---
+++ Conexao.java
@@ -0,0 +1,18 @@
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Conexao {
+    private static final String URL = "jdbc:postgresql://localhost:5432/Banco_de_dados";
+    private static final String USER = "postgres";
+    private static final String PASSWORD = "Pgadmin123";
+
+    public static Connection conectar() {
+        try {
+            return DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (SQLException e) {
+            throw new RuntimeException("Erro na conexão com o banco de dados", e);
+        }
+    }
+}
